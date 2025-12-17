99a1b1aede2f4f13a2d9858cc5ddbf639b57d71f
fengfeng <fengfengzhidao@126.com>
2025-12-17 21:50:00 +0800

    emits

---
+++ src/views/vue组件化开发/2.emits.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import Emits1 from "@/components/emits1.vue";
+import Emits2 from "@/components/emits2.vue";
+
+function submit(val: string) {
+  console.log(val)
+}
+
+function mutiSubmit(val: string, name: string) {
+  console.log(val, name)
+}
+</script>
+
+<template>
+  <div>
+    父组件
+    <div>
+      <emits1 @submit="submit" @muti-submit="mutiSubmit"></emits1>
+    </div>
+    <div>
+      <emits2 @submit="submit" @muti-submit="mutiSubmit"></emits2>
+    </div>
+  </div>
+</template>

923f411013323c75ea56b9d2448ff09cd26c8655
fengfeng <fengfengzhidao@126.com>
2025-12-20 15:59:13 +0800

    vue-router

---
+++ src/views/vue-router/1.vue-router.vue
@@ -0,0 +1,71 @@
+<template>
+  <div>
+    router-vue
+
+    <div>
+      路由跳转
+
+      <div class="nav">
+        <router-link to="/">home</router-link>
+        <router-link to="/about">about</router-link>
+        <router-link to="/admin">admin home</router-link>
+        <router-link to="/admin/info">admin info</router-link>
+        <router-link to="/admin1">admin1 info</router-link>
+        <router-link to="/admin1/info">admin1 info</router-link>
+      </div>
+      <div class="nav">
+        <router-link :to="{name: 'home'}">home</router-link>
+        <router-link :to="{name: 'about'}">about</router-link>
+        <router-link :to="{name: 'articleDetail', params: {id: 1}}">article 1</router-link>
+        <router-link :to="{name: 'articleDetail', params: {id: 2}}">article 2</router-link>
+        <router-link :to="{name: 'articleDetail', params: {id: 3}}">article 3</router-link>
+      </div>
+
+      <div class="nav">
+        <a href="javascript:void 0" @click="goRouter1('home')">home</a>
+        <a href="javascript:void 0" @click="goRouter2('/about')">about</a>
+        <a href="javascript:void 0" @click="goRouter3('adminHome')">admin Home</a>
+      </div>
+
+      <div>
+        动态路由
+        <a href="javascript:void 0" @click="goRouter2('/route')">route</a>
+        <a href="javascript:void 0" @click="goRouter2('/admin1/route')">admin1 route</a>
+        <a href="javascript:void 0" @click="goRouter2('/route1')">route1</a>
+      </div>
+
+      <div class="view">
+        <router-view></router-view>
+      </div>
+
+    </div>
+
+
+
+  </div>
+
+</template>
+
+<script lang="ts" setup>
+import router from "@/router";
+import {useRouter} from "vue-router";
+
+const router1 = useRouter()
+
+
+// 用路由名称跳
+function goRouter1(name: string){
+  router.push({name: name})
+}
+
+// 用路径跳转
+function goRouter2(path: string){
+  router.push(path)
+}
+
+// 使用useRouter实例跳
+function goRouter3(name: string){
+  router1.push({name: name})
+}
+
+</script>

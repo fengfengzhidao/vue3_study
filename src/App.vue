6759651210f911512eb4b4e1c620bdaf8e3e326b
fengfeng <fengfengzhidao@126.com>
2025-12-20 09:33:58 +0800

    环境变量

--- src/App.vue
+++ src/App.vue
@@ -3,5 +3,10 @@
 </template>
 
 <script lang="ts" setup>
-
+console.log(import.meta.env.VITE_SERVER_URL)
+  fetch("http://127.0.0.1:4523/m1/7591032-7329491-default/api/get_data?token=jwtToken",).then(response=>{
+    response.json().then(res=>{
+      console.log(res)
+    })
+  })
 </script>
--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,31 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import { vitePluginForArco } from '@arco-plugins/vite-vue'
 
+interface ImportMetaEnv {
+  VITE_SERVER_URL: string
+}
+
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-    // vitePluginForArco({
-    //   style: 'css'
-    // })
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default defineConfig(({mode})=>{
+  console.log(mode)
+  const env: Record<keyof ImportMetaEnv, string> = loadEnv(mode, process.cwd())
+  console.log(env.VITE_SERVER_URL)
+  return {
+    plugins: [
+      vue(),
+      vueDevTools(),
+      // vitePluginForArco({
+      //   style: 'css'
+      // })
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      },
     },
-  },
+  }
 })
